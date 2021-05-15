c2fb257b12aab8f05b552180fd83a57f7ae1135a
Chama Benlafkih <chamabenlafkih.cb@gmail.com>
2021-05-15 07:29:29 +0200

    tests j-unit

--- be-graphes-algos/src/test/java/org/insa/graphs/algorithm/shortestpath/DijkstraTest.java
+++ be-graphes-algos/src/test/java/org/insa/graphs/algorithm/shortestpath/DijkstraTest.java
@@ -16,6 +16,9 @@ import org.junit.Test;
 public class DijkstraTest{
 	// Small graph use for tests
     private static Graph graph;
+    
+    // list of Points
+    private static Point[] points; 
 
     // List of nodes
     private static Node[] nodes;
@@ -25,13 +28,16 @@ public class DijkstraTest{
     private static Arc a2b, a2c, a2e, b2c, c2d_1, c2d_2, c2d_3, c2a, d2a, d2e, e2d, f2g, e2h, e2i, e2j;
     
     // data examples
-    private static ShortestPathData data1, data2, data3;
+    private static ShortestPathData data1d, data1t, data2d, data2t, data3d, data3t;
     
     // Bellman Ford
-    private static BellmanFordAlgorithm bellman1, bellman2, bellman3;
+    private static BellmanFordAlgorithm bellman1d, bellman1t, bellman2d, bellman2t, bellman3d, bellman3t;
     
     //Dijkstra
-    private static DijkstraAlgorithm dijkstra1, dijkstra2, dijkstra3;
+    private static DijkstraAlgorithm dijkstra1d, dijkstra2d, dijkstra3d, dijkstra1t, dijkstra2t, dijkstra3t;
+    
+    //A*
+    private static AStarAlgorithm astar1d, astar2d, astar3d, astar1t, astar2t, astar3t;
 
 
     @BeforeClass
@@ -42,11 +48,24 @@ public class DijkstraTest{
                 speed20 = new RoadInformation(RoadType.MOTORWAY, null, true, 72, ""),
                 speed30 = new RoadInformation(RoadType.MOTORWAY, null, true, 100, "");
         
-
+        // Create points
+        points= new Point[11];
+        points[0] = new Point(222, 222);
+        points[1] = new Point(235, 127);
+        points[2] = new Point(200, 225);
+        points[3] = new Point(229, 199);
+        points[4] = new Point(192, 222);
+        points[5] = new Point(290, 190);
+        points[6] = new Point(222, 200);
+        points[7] = new Point(217, 230);
+        points[8] = new Point(197, 197);
+        points[9] = new Point(201, 189);
+        points[10] = new Point(130, 240);
+        
         // Create nodes
         nodes = new Node[11];
         for (int i = 0; i < nodes.length; ++i) {
-            nodes[i] = new Node(i, null);
+            nodes[i] = new Node(i, points[i]);
         }
 
         // Add arcs...
@@ -64,45 +83,86 @@ public class DijkstraTest{
         e2h = Node.linkNodes(nodes[4], nodes[8], 35, speed10, null);
         e2i = Node.linkNodes(nodes[4], nodes[9], 45, speed20, null);
         e2j = Node.linkNodes(nodes[4], nodes[10], 10, speed30, null);
-     
-        
         
         //add graph
         graph = new Graph("ID", "", Arrays.asList(nodes), null);
         
         // add data
-        data1= new ShortestPathData(graph, nodes[0], nodes[0], ArcInspectorFactory.getAllFilters().get(0));
-        data2= new ShortestPathData(graph, nodes[0], nodes[1], ArcInspectorFactory.getAllFilters().get(1));
-        data3= new ShortestPathData(graph, nodes[0], nodes[7], ArcInspectorFactory.getAllFilters().get(2));
+        data1d= new ShortestPathData(graph, nodes[0], nodes[0], ArcInspectorFactory.getAllFilters().get(0));
+        data1t= new ShortestPathData(graph, nodes[0], nodes[0], ArcInspectorFactory.getAllFilters().get(2));
+        data2d= new ShortestPathData(graph, nodes[0], nodes[1], ArcInspectorFactory.getAllFilters().get(0));
+        data2t= new ShortestPathData(graph, nodes[0], nodes[1], ArcInspectorFactory.getAllFilters().get(2));
+        data3d= new ShortestPathData(graph, nodes[0], nodes[7], ArcInspectorFactory.getAllFilters().get(0));
+        data3t= new ShortestPathData(graph, nodes[0], nodes[7], ArcInspectorFactory.getAllFilters().get(2));
         
-        //add Bellman Ford and Dijkstra
-        bellman1 = new BellmanFordAlgorithm(data1);
-        bellman2 = new BellmanFordAlgorithm(data2);
-        bellman3 = new BellmanFordAlgorithm(data3);
-        dijkstra1 = new DijkstraAlgorithm(data1);
-        dijkstra2 = new DijkstraAlgorithm(data2);
-        dijkstra3 = new DijkstraAlgorithm(data3);
+        //add Bellman Ford, Dijkstra and A*
+        bellman1d = new BellmanFordAlgorithm(data1d);
+        bellman1t = new BellmanFordAlgorithm(data1t);
+        bellman2d = new BellmanFordAlgorithm(data2d);
+        bellman2t = new BellmanFordAlgorithm(data2t);
+        bellman3d = new BellmanFordAlgorithm(data3d);
+        bellman3t = new BellmanFordAlgorithm(data3t);
+        dijkstra1d = new DijkstraAlgorithm(data1d);
+        dijkstra1t = new DijkstraAlgorithm(data1t);
+        dijkstra2d = new DijkstraAlgorithm(data2d);
+        dijkstra2t = new DijkstraAlgorithm(data2t);
+        dijkstra3d = new DijkstraAlgorithm(data3d);
+        dijkstra3t = new DijkstraAlgorithm(data3t);
+        astar1d = new AStarAlgorithm(data1d);
+        astar1t = new AStarAlgorithm(data1t);
+        astar2d = new AStarAlgorithm(data2d);
+        astar2t = new AStarAlgorithm(data2t);
+        astar2d = new AStarAlgorithm(data2d);
+        astar2t = new AStarAlgorithm(data2t);
         
-    }
-    @Test
-    public void testIsValidD() {
-    	assertTrue(bellman1.doRun().getPath().isValid());
-    	assertTrue(bellman2.doRun().getPath().isValid());
-    	assertTrue(bellman3.doRun().getPath().isValid());
     }
     
     @Test
-    public void testIsValid() {
-    	assertTrue(dijkstra1.doRun().getPath().isValid());
-    	assertTrue(dijkstra2.doRun().getPath().isValid());
-    	assertTrue(dijkstra3.doRun().getPath().isValid());
+    public void testIsValidDijskstra() {
+    	//distance
+    	assertTrue(dijkstra1d.doRun().getPath().isValid()); //origine=destination
+    	assertTrue(dijkstra2d.doRun().getPath().isValid()); //solution possible
+    	assertTrue(dijkstra3d.doRun().getPath().isValid()); //graphe non connexe
+    	//temps
+    	assertTrue(dijkstra1t.doRun().getPath().isValid()); //origine=destination
+    	assertTrue(dijkstra2t.doRun().getPath().isValid()); //solution possible
+    	assertTrue(dijkstra3t.doRun().getPath().isValid()); //graphe non connexe
     }
     
     @Test
-    public void testBellmanFord() {
-        assertEquals(bellman1.doRun().getPath(), dijkstra1.doRun().getPath());
-        assertEquals(bellman2.doRun().getPath(), dijkstra2.doRun().getPath());
-        assertEquals(bellman3.doRun().getPath(), dijkstra3.doRun().getPath());
+    public void testIsValidAstar() {
+    	//distance
+    	assertTrue(astar1d.doRun().getPath().isValid());
+    	assertTrue(astar2d.doRun().getPath().isValid());
+    	assertTrue(astar3d.doRun().getPath().isValid());
+    	//temps
+    	assertTrue(astar1t.doRun().getPath().isValid());
+    	assertTrue(astar2t.doRun().getPath().isValid());
+    	assertTrue(astar3t.doRun().getPath().isValid());
+    }
+    
+    @Test
+    public void testDijkstra() {
+    	//distance
+        assertEquals(bellman1d.doRun().getPath().getArcs(), dijkstra1d.doRun().getPath().getArcs());
+        assertEquals(bellman2d.doRun().getPath().getArcs(), dijkstra2d.doRun().getPath().getArcs());
+        assertEquals(bellman3d.doRun().getPath().getArcs(), dijkstra3d.doRun().getPath().getArcs());
+        //temps
+        assertEquals(bellman1t.doRun().getPath().getArcs(), dijkstra1d.doRun().getPath().getArcs());
+        assertEquals(bellman2t.doRun().getPath().getArcs(), dijkstra2d.doRun().getPath().getArcs());
+        assertEquals(bellman3t.doRun().getPath().getArcs(), dijkstra3d.doRun().getPath().getArcs());
+    }
+    
+    @Test
+    public void testAstar() {
+    	//distance
+        assertEquals(bellman1d.doRun().getPath().getArcs(), astar1d.doRun().getPath().getArcs());
+        assertEquals(bellman2d.doRun().getPath().getArcs(), astar2d.doRun().getPath().getArcs());
+        assertEquals(bellman3d.doRun().getPath().getArcs(), astar3d.doRun().getPath().getArcs());
+        //temps
+        assertEquals(bellman1t.doRun().getPath().getArcs(), astar1d.doRun().getPath().getArcs());
+        assertEquals(bellman2t.doRun().getPath().getArcs(), astar2d.doRun().getPath().getArcs());
+        assertEquals(bellman3t.doRun().getPath().getArcs(), astar3d.doRun().getPath().getArcs());
     }
 
 }
